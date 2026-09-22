245e57fb7bd7502a966377f9ee73019f31558cb2
Jlopezcha <lopezchaparro55@gmail.com>
2026-09-21 21:43:12 -0400

    aws updates

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail # stop at the first failure
 
-HOST=ec2-user@3.23.70.178
+HOST=ec2-user@3.12.144.166
 KEY=~/.ssh/quotio.pem
 npm run build # on your laptop. 1 GB cannot do this.
 
 rsync -avz --delete \
---exclude '.git' --exclude 'node_modules' --exclude '.env' --exclude '.venv' \
+--exclude '.git' --exclude 'node_modules' --exclude '.env' --exclude 'src/rag_ai_service/venv' --exclude 'src/rag_ai_service/__pycache__cd' \
 -e "ssh -i $KEY" ./ "$HOST":/var/www/app/
 
 ssh -i "$KEY" "$HOST" \
